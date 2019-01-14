489cee6480c45e81e6518dfa5898d9bbe5430779
Kutmanai <zkutmanai@gmail.com>
2019-01-13 20:07:51 -0600

    updated by Kutmanai

--- src/test/java/ItestResultts/TestSteps.java
+++ src/test/java/ItestResultts/TestSteps.java
@@ -3,5 +3,6 @@ package ItestResultts;
 public class TestSteps {
     public static void main(String[] args) {
         System.out.println(" Java is Fun ");
+        System.out.println("Helooo");
     }
 }
